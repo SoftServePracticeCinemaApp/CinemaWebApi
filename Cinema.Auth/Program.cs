1720e3882a3c37b1c85621ef15b79c3548793be0
OleksanderPylypchuk <o.s.pylypchuk@student.khai.edu>
2025-01-21 21:50:20 +0200

    JwtOptions, Token Generator and IAuthService

--- Cinema.Auth/Program.cs
+++ Cinema.Auth/Program.cs
@@ -1,9 +1,12 @@
+using Cinema.Business.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("ApiSettings:JwtOptions"));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
