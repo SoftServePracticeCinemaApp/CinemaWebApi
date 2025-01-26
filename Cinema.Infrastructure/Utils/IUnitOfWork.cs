985a712979e55c343e8346b8c4e701609ca6b0e2
UkrainetsNazar <ukrnazar51@gmail.com>
2025-01-26 15:47:28 +0200

    Add UnitOfWork

---
+++ Cinema.Infrastructure/Utils/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using Cinema.Domain.Interfaces;
+
+namespace Cinema.Infrastructure.Utils
+{
+    interface IUnitOfWork : IDisposable
+    {
+        IHallRepository HallRepository { get; }
+        IMovieRepository MovieRepository { get; }
+        ISessionRepository SessionRepository { get; }
+        ITicketRepository TicketRepository { get; }
+        Task CompleteAsync();
+    }
+}
---
+++ Cinema.Infrastructure/Utils/UnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using Cinema.Domain.Interfaces;
+
+namespace Cinema.Infrastructure.Utils
+{
+    class UnitOfWork : IUnitOfWork
+    {
+        private readonly CinemaDbContext _context;
+        public IHallRepository HallRepository { get; }
+        public IMovieRepository MovieRepository { get; }
+        public ISessionRepository SessionRepository { get; }
+        public ITicketRepository TicketRepository { get; }
+
+        public UnitOfWork(CinemaDbContext context,
+                          IHallRepository hallRepository,
+                          IMovieRepository movieRepository,
+                          ISessionRepository sessionRepository,
+                          ITicketRepository ticketRepository)
+        {
+            _context = context;
+            HallRepository = hallRepository;
+            MovieRepository = movieRepository;
+            SessionRepository = sessionRepository;
+            TicketRepository = ticketRepository;
+        }
+
+        public async Task CompleteAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
