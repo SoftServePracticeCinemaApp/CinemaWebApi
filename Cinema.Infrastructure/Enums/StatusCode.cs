413e25d6fcd46c2735d140229891dbbd18e388b5
UkrainetsNazar <ukrnazar51@gmail.com>
2025-01-28 17:03:12 +0200

    Add response class

---
+++ Cinema.Infrastructure/Enums/StatusCode.cs
@@ -0,0 +1,10 @@
+﻿namespace Cinema.Application.Enums
+{
+    public enum StatusCode
+    {
+        Ok = 200,
+        NotFound = 404,
+        BadRequest = 400,
+        InternalServerError = 500
+    }
+}
---
+++ Cinema.Infrastructure/Utils/MainResponse.cs
@@ -0,0 +1,12 @@
+﻿using Cinema.Application.Enums;
+
+namespace Cinema
+{
+    public class BaseResponse<T>
+    {
+        public string Description { get; set; } = null!;
+        public StatusCode StatusCode { get; set; }
+        public int ResultsCount { get; set; }
+        public T? Data { get; set; }
+    }
+}
