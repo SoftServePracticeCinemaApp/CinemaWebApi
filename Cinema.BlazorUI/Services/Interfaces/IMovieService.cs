f046361e4bf5da8531783a07442b640cad9d43f7
Swayze <kovaloff10@gmail.com>
2025-02-15 21:38:15 +0200

    Add rating

--- Cinema.BlazorUI/Services/Interfaces/IMovieService.cs
+++ Cinema.BlazorUI/Services/Interfaces/IMovieService.cs
@@ -10,5 +10,8 @@ public interface IMovieService
   Task<MovieResult> GetMovieDataAsync(int id);
   Task<string> GetMovieTrailerAsync(int id);
   Task<List<string>> GetMovieActorsAsync(int id);
+  Task<bool> RateMovieAsync(int movieId, int rating);
+  Task<double?> GetUserRatingAsync(int movieId);
+  Task<bool> HasUserRatedMovieAsync(int movieId);
 }
 
